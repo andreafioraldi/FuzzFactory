08609ec75f59542fbbd9ef0317e975fdcad43ba7
Andrea Fioraldi <andreafioraldi@gmail.com>
2020-07-20 16:58:41 +0200

    handle mem* functions in buf waypoint

--- llvm_mode/waypoints-buf-pass.cc
+++ llvm_mode/waypoints-buf-pass.cc
@@ -11,6 +11,7 @@ class BufFeedback : public DomainFeedback<BufFeedback> {
 public:
     BufFeedback(Module& M) : DomainFeedback<BufFeedback>(M, "__afl_buf_start_dsf") {
     
+      DL = new DataLayout(&M);
       int LongSize = M.getDataLayout().getPointerSizeInBits();
       
       IntTypeSized[0] = getIntTy(8);
@@ -21,9 +22,10 @@ public:
 
       HandleMallocFn = resolveFunction("__afl_buf_handle_malloc", VoidTy, {Int32Ty, getIntPtrTy(8), SizeType});
       HandleCallocFn = resolveFunction("__afl_buf_handle_calloc", VoidTy, {Int32Ty, getIntPtrTy(8), SizeType, SizeType});
+      HandleReallocFn = resolveFunction("__afl_buf_handle_realloc", VoidTy, {Int32Ty, getIntPtrTy(8), getIntPtrTy(8), SizeType});
       HandleFreeFn = resolveFunction("__afl_buf_handle_free", VoidTy, {getIntPtrTy(8)});
       
-      AccessFn = resolveFunction("__afl_buf_access", VoidTy, {getIntPtrTy(8)});
+      AccessFn = resolveFunction("__afl_buf_access", VoidTy, {getIntPtrTy(8), Int32Ty});
     
     }
 
@@ -46,11 +48,41 @@ public:
             auto ptr = irb.CreatePointerCast(&call, getIntPtrTy(8));
 
             irb.CreateCall(HandleCallocFn, {key, ptr, len, cnt}); 
+        } else if (callee->getName() == "realloc") { // Handle realloc
+            auto key = createProgramLocation(); // static random value
+            auto irb = insert_after(call); // Get a handle to the LLVM IR Builder at this point
+            auto old_ptr = irb.CreatePointerCast(call.getArgOperand(0), getIntPtrTy(8));
+            auto bytes = irb.CreateBitCast(call.getArgOperand(1), SizeType);
+            auto ptr = irb.CreatePointerCast(&call, getIntPtrTy(8));
+
+            irb.CreateCall(HandleReallocFn, {key, old_ptr, ptr, bytes}); 
         } else if (callee->getName() == "free") { // Handle free
             auto irb = insert_after(call); // Get a handle to the LLVM IR Builder at this point
             auto ptr = irb.CreatePointerCast(call.getArgOperand(0), getIntPtrTy(8));
 
             irb.CreateCall(HandleFreeFn, {ptr}); 
+        } else if (callee->getName() == "memcpy") { // Handle memcpy
+            auto irb = insert_after(call); // Get a handle to the LLVM IR Builder at this point
+            auto dst = irb.CreatePointerCast(call.getArgOperand(0), getIntPtrTy(8));
+            auto src = irb.CreatePointerCast(call.getArgOperand(1), getIntPtrTy(8));
+            auto size = irb.CreateBitCast(call.getArgOperand(2), SizeType);
+
+            irb.CreateCall(AccessFn, {src, size}); 
+            irb.CreateCall(AccessFn, {dst, size});
+        } else if (callee->getName() == "memmove") { // Handle memmove
+            auto irb = insert_after(call); // Get a handle to the LLVM IR Builder at this point
+            auto dst = irb.CreatePointerCast(call.getArgOperand(0), getIntPtrTy(8));
+            auto src = irb.CreatePointerCast(call.getArgOperand(1), getIntPtrTy(8));
+            auto size = irb.CreateBitCast(call.getArgOperand(2), SizeType);
+
+            irb.CreateCall(AccessFn, {src, size}); 
+            irb.CreateCall(AccessFn, {dst, size});
+        } else if (callee->getName() == "memset") { // Handle memset
+            auto irb = insert_after(call); // Get a handle to the LLVM IR Builder at this point
+            auto dst = irb.CreatePointerCast(call.getArgOperand(0), getIntPtrTy(8));
+            auto size = irb.CreateBitCast(call.getArgOperand(2), SizeType);
+
+            irb.CreateCall(AccessFn, {dst, size});
         }
     }
     
@@ -58,17 +90,24 @@ public:
       auto irb = insert_after(inst);
       auto ptr = irb.CreatePointerCast(inst.getPointerOperand(), getIntPtrTy(8));
 
-      irb.CreateCall(AccessFn, {ptr}); 
+      PointerType* PT = cast<PointerType>(inst.getPointerOperand()->getType());                         
+      auto size = getConst(DL->getTypeStoreSize(PT->getPointerElementType()));
+
+      irb.CreateCall(AccessFn, {ptr, size}); 
     }
     
     void visitLoadInst(LoadInst& inst) {
       auto irb = insert_after(inst);
       auto ptr = irb.CreatePointerCast(inst.getPointerOperand(), getIntPtrTy(8));
 
-      irb.CreateCall(AccessFn, {ptr}); 
+      PointerType* PT = cast<PointerType>(inst.getPointerOperand()->getType());                         
+      auto size = getConst(DL->getTypeStoreSize(PT->getPointerElementType()));
+
+      irb.CreateCall(AccessFn, {ptr, size}); 
     }
 
 private:
+    DataLayout* DL;
     Type *IntTypeSized[4];
     Type *SizeType;
     Function* HandleMallocFn, *HandleCallocFn, *HandleReallocFn, *HandleFreeFn, *AccessFn;
--- llvm_mode/waypoints-buf-rt.c
+++ llvm_mode/waypoints-buf-rt.c
@@ -53,14 +53,14 @@ void __afl_buf_alloc_tree_remove(uintptr_t start, uintptr_t end) {
   }
 }
 
-void __afl_buf_access(uintptr_t ptr) {
+void __afl_buf_access(uintptr_t ptr, uint32_t size) {
   uintptr_t start, end, alloc_site;
   if (!__afl_buf_alloc_tree_search(ptr, &start, &end, &alloc_site)) return;
   uintptr_t k = (uintptr_t)__builtin_return_address(0);
   k = (k >> 4) ^ (k << 8) ^ alloc_site;
   k &= BUF_MAP_SIZE - 1;
   FUZZFACTORY_DSF_MAX(__afl_buf_start_dsf, k, (uint32_t)(start - ptr));
-  FUZZFACTORY_DSF_MAX(__afl_buf_end_dsf, k, (uint32_t)(ptr - end));
+  FUZZFACTORY_DSF_MAX(__afl_buf_end_dsf, k, (uint32_t)((ptr + size) - end));
 }
 
 void __afl_buf_handle_malloc(uint32_t k, uintptr_t ptr, uintptr_t len) {
